22b9086a43eacf4fb96d27057d8847b211b0d16c
bohdan062282 <59960025+bohdan062282@users.noreply.github.com>
2025-04-06 23:41:46 +0300

    CharacterController_3_2_3
    -perspective_1_3

--- Assets/Player/Scripts/Perspective.cs
+++ Assets/Player/Scripts/Perspective.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Perspective : MonoBehaviour
 {
 
+    public GameObject sphere;
+
     [SerializeField] private CinemachineCamera firstPersonCamera;
     [SerializeField] private CinemachineCamera thirdPersonCamera;
 
@@ -76,7 +78,28 @@ public class Perspective : MonoBehaviour
     }
     private void processSight()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(firstPersonCamera.gameObject.transform.position,
+                            firstPersonCamera.gameObject.transform.forward, out hit, 20.0f))
+        {
+            Vector3 screenPos = _mainCamera.WorldToScreenPoint(hit.point);
+            Resolution resolution = Screen.currentResolution;
+
+            Vector2 vec = new Vector2(screenPos.x - (resolution.width / 2.0f), screenPos.y - (resolution.height / 2.0f));
+            sight.anchoredPosition = vec;
+            Debug.Log(vec);
+            sphere.transform.position = hit.point;
+        }
+        else
+        {
+            Vector3 screenPos = _mainCamera.WorldToScreenPoint(firstPersonCamera.gameObject.transform.position + firstPersonCamera.gameObject.transform.forward.normalized * 21.0f);
+            Resolution resolution = Screen.currentResolution;
+
+            Vector2 vec = new Vector2(screenPos.x - (resolution.width / 2.0f), screenPos.y - (resolution.height / 2.0f));
+            sight.anchoredPosition = vec;
+            Debug.Log(vec);
+            sphere.transform.position = hit.point;
+        }
     }
 
 
